af5dc07bbb0abab4865c297de148097df60e02b1
Amr Mansour <amrmansour217@gmail.com>
2025-05-06 02:34:13 +0300

    feat: Center loader component with flexbox and set minimum height

--- src/components/louder.jsx
+++ src/components/louder.jsx
@@ -14,9 +14,17 @@ const Loader = () => {
 }
 
 const StyledWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  min-height: 200px;
+
   .loader {
     width: 112px;
     height: 112px;
+    position: relative;
   }
 
   .box1,
